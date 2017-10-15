283bf0fe825b91e7fb3581af7765d0cf557c246f
Matthew Doran <mattddoran@gmail.com>
2017-10-15 16:13:00 -0600

    fixed foriegn key in personProfessions

--- extraMetadataTsvtocsv.py
+++ extraMetadataTsvtocsv.py
@@ -9,9 +9,18 @@ def main():
 	with open("extraMetadataTest.txt", "r") as f:
 		for line in f.readlines()[1:]:
 			# extract metadata fields (rank, title, description, votes, metascore)
-			line = line.split("\"")
+			line = line.split("\",")
+			rankTitle = line[0]
+			rankTitle = rankTitle.split(",")
+			description = line[1]
+			description = description.split(",\"")
+			description = description[0]
+			description = description.split(".,")
+			description = description[0]
+			description = description.replace("\"","")
 			print(line)
-			print("!!@!@!@!@!@!@!@!@!" + line[2])
+			print("\\n,{},{},{}\n".format(rankTitle[0],rankTitle[1], description))
+			print("----------------------")
 			#out1.write("\\n,{},{},{},{},{}\n".format(line[0],line[1],line[3],line[9],line[11]))
 
 		out1.close()
