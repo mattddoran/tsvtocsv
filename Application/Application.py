6864715ba133dd65de616b3c3682f58e0532c4c9
Carl Fee <15cfee@gmail.com>
2017-11-24 19:25:37 -0700

    added-proper-files

---
+++ Application/Application.py
@@ -0,0 +1,17 @@
+# acts as a facade
+class Organizer(object):
+    @staticmethod
+    def runQ(qNumber):
+        print "needs to be implemented"
+
+
+#Should take care of the user choosing the question
+# return question number or -1 for exit
+def menu():
+    return 1
+if __name__ == '__main__':
+    print "running"
+    Organizer.runQ(3)
+
+
+
