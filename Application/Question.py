76b53cf356a59805a6e869990be1e164d8958929
Carl Fee <15cfee@gmail.com>
2017-11-24 19:11:53 -0700

    question

--- Application/Question.py
+++ Application/Question.py
@@ -1,9 +1,17 @@
 from interfaces import Visualization, Analysis, Query
 
+#Inherits these three classes as interfaces
 class Question(Visualization,Analysis,Query):
 
-    def __init__(self, title):
+    def __init__(self, title, data):
         self.id = title
+        self.data = data
 
-    def doSome(self):
-        print "sup"
+    def displayImage(self):
+        print "Matt is so cool"
+
+    def displayAnalysis(self):
+        print "Movies are hip"
+        
+    def userFilter(self):
+        print "Treat your self"
