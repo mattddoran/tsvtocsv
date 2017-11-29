14eb90d09c20d246e9e17d21bd152e074ef5bcb5
Carl Fee <15cfee@gmail.com>
2017-11-29 13:34:24 -0700

    working-full-query

--- Application/Q4.py
+++ Application/Q4.py
@@ -4,9 +4,9 @@ import mysql.connector
 class Q4(Question):
     @staticmethod
     def query():
-        cnx = mysql.connector.connect(user='root', password='Rrevolution@1', host='127.0.0.1', database='mydb2', buffered='true')
+        cnx = mysql.connector.connect(user='root', password='Rrevolution@1', host='127.0.0.1', database='mydb2')
 
-        cursor = cnx.cursor()
+        cursor = cnx.cursor(buffered=True)
         #
         query = ("SELECT primaryTitle,originalTitle "
                  "FROM title "
@@ -17,26 +17,26 @@ class Q4(Question):
 
 
         query =(
-        "select mydb2.genres.genre, count(*) as 'Number of Titles', sum(mydb2.rating.numVotes) as 'Sum of votes for all Titles in this Genre'"
-        "from mydb2.genres cross join mydb2.titlegenres cross join mydb2.title cross join mydb2.rating"
-        "where mydb2.genres.idGenre = mydb2.titlegenres.Genres_idGenre and mydb2.title.idTitle = mydb2.titlegenres.Title_idTitle and mydb2.rating.Title_idTitle = mydb2.title.idTitle and mydb2.rating.averageRating < 2 and mydb2.rating.numVotes > 10"
-        "group by mydb2.genres.idGenre order by sum(rating.numVotes) Desc;")
+        "select mydb2.genres.genre, count(*) as 'Number of Titles', sum(mydb2.rating.numVotes) as 'Sum of votes for all Titles in this Genre' "
+        "from mydb2.genres cross join mydb2.titlegenres cross join mydb2.title cross join mydb2.rating "
+        "where mydb2.genres.idGenre = mydb2.titlegenres.Genres_idGenre and mydb2.title.idTitle = mydb2.titlegenres.Title_idTitle and mydb2.rating.Title_idTitle = mydb2.title.idTitle and mydb2.rating.averageRating < 2 and mydb2.rating.numVotes > 10 "
+        "group by mydb2.genres.idGenre order by sum(rating.numVotes) Desc; ")
 
         # cursor.execute()
 
         cursor.execute(query)
-        #
-        # list = []
-        # num = 0
-        # print 'executed'
-        # for (isTitle, a) in cursor:
-        #     # print isTitle, a
-        #     # print("{}".format(isTitle))
-        #     num += 1
-        #     if (num % 100000 == 0):
-        #         print num
-        #         list.append(isTitle)
-        # print list
-        # cursor.close()
+
+        list = []
+        num = 0
+        print 'executed'
+        for (isTitle, a, b) in cursor:
+            # print isTitle, a
+            # print("{}".format(isTitle))
+            num += 1
+            if (num % 1 == 0):
+                print num
+                list.append(isTitle)
+        print list
+        cursor.close()
 
         cnx.close()
