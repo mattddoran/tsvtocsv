5ccea975375d81ccfa64f89205ebbd1ce9bc4883
Carl Fee <15cfee@gmail.com>
2017-12-01 18:09:10 -0700

    starting-to-add-runQuery

--- Application/Q4.py
+++ Application/Q4.py
@@ -4,29 +4,14 @@ import mysql.connector
 class Q4(Question):
     @staticmethod
     def query():
-        cnx = mysql.connector.connect(user='root', password='Rrevolution@1', host='127.0.0.1', database='mydb2')
-
-        cursor = cnx.cursor(buffered=True)
-
-
         query =(
         "select mydb2.genres.genre, mydb2.title.startYear, count(*) as 'Number of Titles' "
         "from mydb2.genres cross join mydb2.titlegenres cross join mydb2.title cross join mydb2.rating "
         "where mydb2.genres.idGenre = mydb2.titlegenres.Genres_idGenre and mydb2.title.idTitle = mydb2.titlegenres.Title_idTitle "
         "and mydb2.rating.Title_idTitle = mydb2.title.idTitle "
         "group by mydb2.genres.idGenre, mydb2.title.startYear;")
-
-        # cursor.execute()
-
-        cursor.execute(query)
-
-        list = []
-        num = 0
-        print 'executed'
-        for (genre, year, count) in cursor:
+        data = Question.runQuery(query)
+        print 'Finished'
+        for (genre, year, count) in data:
             # print isTitle, a
              print("{},{},{}".format(genre,year,count))
-        print list
-        cursor.close()
-
-        cnx.close()
--- Application/Question.py
+++ Application/Question.py
@@ -1,4 +1,5 @@
 from interfaces import Visualization, Analysis, Query
+import mysql.connector
 
 #Inherits these three classes as interfaces
 class Question(Visualization,Analysis,Query):
@@ -15,3 +16,25 @@ class Question(Visualization,Analysis,Query):
 
     def userFilter(self):
         print ("Treat your self")
+
+    @staticmethod
+    def runQuery(query):
+        cnx = mysql.connector.connect(user='root', password='Rrevolution@1', host='127.0.0.1', database='mydb2')
+        cursor = cnx.cursor(buffered=True)
+        cursor.execute(query)
+        data = cursor
+        print 0
+        for (genre, year, count) in cursor:
+            # print isTitle, a
+             print("{},{},{}".format(genre,year,count))
+        print 1
+        for (genre, year, count) in data:
+            # print isTitle, a
+             print("{},{},{}".format(genre,year,count))
+        cursor.close()
+        cnx.close()
+        print 2
+        for (genre, year, count) in data:
+            # print isTitle, a
+             print("{},{},{}".format(genre,year,count))
+        return cursor
